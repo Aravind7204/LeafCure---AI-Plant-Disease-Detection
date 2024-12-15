697ee3051ec33933e700b66cac67943944069e23
Aravind7204 <107171394+Aravind7204@users.noreply.github.com>
2024-12-15 16:37:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,11 +115,7 @@ def get_solution():
     disease_name = request.json.get('disease_name')  # Ensure the correct key name
     solution = solutions.get(disease_name, "Solution not found for this disease.")
     return jsonify({"disease_name": disease_name, "solution": solution})
-
-if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False,host='0.0.0.0', port=port)
 
 
 
