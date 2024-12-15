be72942678972d6516982e24883b3b393a683ade
Aravind7204 <107171394+Aravind7204@users.noreply.github.com>
2024-12-15 15:13:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -116,6 +116,9 @@ def get_solution():
     solution = solutions.get(disease_name, "Solution not found for this disease.")
     return jsonify({"disease_name": disease_name, "solution": solution})
 
+if __name__ == '__main__':
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    app.run(debug=False,host='0.0.0.0')
 
 
 
